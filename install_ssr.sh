c7ab85917bd0b879e322ba2e771269a253770017
lete01 <87194323+lete01@users.noreply.github.com>
2025-03-25 22:40:59 +0800

    Update install_ssr.sh

--- install_ssr.sh
+++ install_ssr.sh
@@ -1,19 +1,6 @@
-#!/bin/bash
+# 安装wget
+yum -y install wget
 
-# 安装wget和expect
-yum -y install wget expect
-apt-get update && apt-get install -y expect
-# 下载SSR脚本
+# 下载并运行SSR脚本，自动输入选项
 wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/ssr.sh && chmod +x ssr.sh
-
-# 使用expect自动交互
-expect <<EOF
-spawn bash ssr.sh
-expect "请输入数字" { send "1\r" }
-expect "请输入端口" { send "618\r" }
-expect "请输入密码" { send "618\r" }
-expect "请输入要设置的加密方式" { send "7\r" }
-expect "请输入要设置的协议插件" { send "1\r" }
-expect "请输入要设置的混淆插件" { send "1\r" }
-expect eof
-EOF
+echo -e "1\n618\n618\n7\n1\n1\n" | bash ssr.sh
